e6db74b0716aafc81f041cc433e4c1ed478e861c
Gil Forcada Codinachs <gil.gnome@gmail.com>
2024-02-22 19:34:50 +0100

    feat: switch to pathlib

--- yarn_build.py
+++ yarn_build.py
@@ -1,10 +1,9 @@
 """zest.releaser plugin to build JavaScript projects"""
 import logging
-import os
 import subprocess
 import sys
 from configparser import ConfigParser, NoOptionError, NoSectionError
-from os.path import join
+from pathlib import Path
 
 from zest.releaser.utils import ask
 
@@ -12,13 +11,13 @@ logger = logging.getLogger("yarn.build")
 
 
 def get_configured_location(path):
-    setup_cfg = os.sep.join([path, "setup.cfg"])
-    if os.path.exists(setup_cfg):
+    setup_cfg = path / "setup.cfg"
+    if setup_cfg.exists():
         config = ConfigParser()
         config.read(setup_cfg)
         try:
-            folder_path = config.get("yarn.build", "folder")
-            if os.path.exists(folder_path):
+            folder_path = Path(config.get("yarn.build", "folder"))
+            if folder_path.exists():
                 return folder_path
             logger.warning(f"{folder_path} does not exist")
         except NoSectionError:
@@ -40,14 +39,13 @@ def find_package_json(path):
 
 
 def recursive_find_package_json(path):
-    """Find a ``packages.json`` file and run yarn on it"""
-    for filename in os.listdir(path):
-        dir_path = join(path, filename)
-        if filename == "package.json":
+    """Find a `packages.json` file and run yarn on it"""
+    for file_obj in path.iterdir():
+        if file_obj.name == "package.json":
             logger.info("yarn: package.json found!")
             return path
-        elif os.path.isdir(dir_path):
-            recursive_find_package_json(dir_path)
+        elif file_obj.is_dir():
+            recursive_find_package_json(file_obj)
 
     return None
 
@@ -82,7 +80,7 @@ def build_project(data):
         return
     logger.debug(f"yarn: Find and build JavaScript projects on {tagdir}")
     try:
-        location = find_package_json(tagdir)
+        location = find_package_json(Path(tagdir))
         if location:
             build(location)
     except Exception:  # noqa: B902
